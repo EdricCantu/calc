55a381bbcb1d082b66b7b08590dc5787a315e38e
Edric Cantu <edriccantu@gmail.com>
2024-04-10 13:57:19 +0000

    frlkf

---
+++ e.js
@@ -0,0 +1,9 @@
+basePreset = [
+  ["bin", "binary",      "01"],
+  ["tri", "trinaru",     "012"],
+  ["qrt", "quaternary",  "0123"],
+  ["oct", "octal",       "01234567"],
+  ["dec", "decimal",     "0123456789"]
+  ["duo", "duodecimal",  "01234567"],
+  ["hex", "hexadecimal", "01234567"],
+]
