3326d7d501b951db60084bb1d0886f46beb8ac17
Edric Cantu <edriccantu@gmail.com>
2024-04-11 13:55:12 +0000

    l

---
+++ geometry.js
@@ -0,0 +1,25 @@
+class Sphere{
+  constructor(){
+    
+  }
+
+  #_c;
+  #_a;
+  #_r;
+  #_d;
+  set c(c){//circumference
+    this._c = c;
+    this._r = c / (2*Math.PI);
+    this._d = c / Math.PI;
+    this._a = 
+  }
+  set a(a){//surface area
+    
+  }
+  set r(r){//radius
+    
+  }
+  set d(d){//diameter
+    
+  }
+}
